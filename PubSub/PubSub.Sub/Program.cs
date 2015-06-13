254b18bae008bc73336a25e2b602c81d2c43138c
Radu <flo.radu_88@yahoo.com>
2015-06-13 16:27:34 +0100

    Added pubsub basic example from http://zguide.zeromq.org/cs:psenvsub and http://zguide.zeromq.org/cs:psenvpub

---
+++ PubSub/PubSub.Sub/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ZeroMQ;
+
+namespace PubSub.Sub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            // Pubsub envelope subscriber
+            //
+            // Author: metadings
+            //
+
+            // Prepare our context and subscriber
+            using (var context = new ZContext())
+            using (var subscriber = new ZSocket(context, ZSocketType.SUB))
+            {
+                subscriber.Connect("tcp://127.0.0.1:5563");
+                subscriber.Subscribe("B");
+
+                while (true)
+                {
+                    using (ZMessage message = subscriber.ReceiveMessage())
+                    {
+                        // Read envelope with address
+                        string address = message[0].ReadString();
+
+                        // Read message contents
+                        string contents = message[1].ReadString();
+
+                        Console.WriteLine("[{0}] {1}", address, contents);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PubSub/PubSub/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using ZeroMQ;
+
+namespace PubSub
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //
+            // Pubsub envelope publisher
+            //
+            // Author: metadings
+            //
+
+            // Prepare our context and publisher
+            using (var context = new ZContext()) {
+                using (var publisher = new ZSocket(context, ZSocketType.PUB))
+                {
+                    publisher.Linger = TimeSpan.Zero;
+                    publisher.Bind("tcp://*:5563");
+
+                    while (true)
+                    {
+                        // Write two messages, each with an envelope and content
+                        using (var message = new ZMessage())
+                        {
+                            message.Add(new ZFrame("A"));
+                            message.Add(new ZFrame("We don't want to see this"));
+                            publisher.Send(message);
+                        }
+                        using (var message = new ZMessage())
+                        {
+                            message.Add(new ZFrame("B"));
+                            message.Add(new ZFrame("We would like to see this"));
+                            publisher.Send(message);
+                        }
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+        }
+    }
+}
